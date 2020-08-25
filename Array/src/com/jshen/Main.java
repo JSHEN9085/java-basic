074430a7fcba0501d56f1f764e9926b58d5d8988
JSHEN9085 <jshen9085@gamil.com>
2020-08-25 14:10:13 -0400

    array

--- Array/src/com/jshen/Main.java
+++ Array/src/com/jshen/Main.java
@@ -1,4 +1,5 @@
 package com.jshen;
+import java.util.*;
 
 public class Main {
 /*
@@ -34,6 +35,65 @@ System.out.println(intArray[0] == cloneArray[0]);  -- True
 System.out.println(intArray[1] == cloneArray[1]);  -- True
 */
     public static void main(String[] args) {
-	// write your code here
+        int intArr[] = { 10, 20, 15, 22, 35 };
+
+        //sorting
+        //sort(originalArray, fromIndex, endIndex), can also specify the range of sort
+        System.out.println("before sort");
+        System.out.println(intArr[2]);
+        Arrays.sort(intArr); // will change the original input
+        System.out.println("after sort");
+        System.out.println(intArr[2]);
+
+        //to String
+        //using Arrays.toString, if directly print intArr, we will have un readable result
+        System.out.println(Arrays.toString(intArr));
+
+        //finding index
+        //there is no direct method of indexOf in array. That is for Arraylist
+        System.out.println(Arrays.binarySearch(intArr, 10));
+
+        //Binary Search
+        //binarySearch(int[] array, int fromIndex, int toIndex, T key)
+        //method searches a range of the specified array for the specified object using the binary search algorithm
+        System.out.println(Arrays.binarySearch(intArr, 1, 3, 20)); //it found 20 is in index of 2
+
+        //Compare
+        int[] input1 = {0, 1, 2, 3};
+        int[] input2 = new int[] {0, 1, 2, 3};
+        int[] input3 = input1;
+        System.out.println(input1 == input2); //false, because 1 and 2 are pointing to different address
+        System.out.println(input1 == input3); //true, because 1 and 3 are pointing to same address
+        System.out.println(Arrays.equals(input1, input2)); //true, because 1 & 2 have same numbers
+        System.out.println(Arrays.compare(input1, input2)); //compare method will show how many position is different, not which position is different
+
+        //deep equal and deepToString
+        //deepEquals(Object[] a1, Object[] a2), compare 2d or more, but only for numbers
+        int int2d[][] = { { 10, 20, 15, 22, 35 } };
+        int int2dArr[][] = { { 10, 20, 15, 22, 35 } };
+        System.out.println(Arrays.deepEquals(int2d, int2dArr)); //true
+        System.out.println(Arrays.deepToString(int2d)); //using deepToString to print 2d array
+
+
+        //Copy
+        //copyOf(originalArray, newLength)
+        int[] intArr2 = Arrays.copyOf(intArr, 10); //the extra length will be default as 0
+        System.out.println(intArr == intArr2); //false, as address is different
+        System.out.println(Arrays.toString(intArr2));
+        //copyOfRange(originalArray, fromIndex, endIndex)
+        int[] intArr3 = Arrays.copyOfRange(intArr, 0, 2); //to index is excluded, so only copy index of 0 and 1
+        System.out.println(Arrays.toString(intArr3));
+
+        //fill
+        //Arrays.fill will replace all index instead of 1
+        int[] original = new int[]{1, 2, 3, 4, 5};
+        Arrays.fill(original, 10);
+        System.out.println(Arrays.toString(original));
+
+        //mismatch
+        int[] arr1 = new int[]{1, 2, 3, 4};
+        int[] arr2 = new int[]{1, 2, 4};
+        System.out.println(Arrays.mismatch(arr1, arr2)); //2, mismatch will show which position is not matched
+
     }
 }
